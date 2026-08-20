7a00b2ad27cfbba9c811bd35b4c171a96a6fe073
juliatarifamendes-wq <julia.tarifa.mendes@escola.pr.gov.br>
2026-08-20 10:43:31 -0300

    ok

---
+++ script.js
@@ -0,0 +1,35 @@
+// Botões de acessibilidade
+document.addEventListener("DOMContentLoaded", () => {
+
+const btnContraste = document.getElementById("btn-contraste");
+const btnAumentar = document.getElementById("btn-aumentar-texto");
+const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+// Tamanho inicial da fonte em porcentagem
+let tamanhoAtualFonte = 100;
+
+// Função de alto contraste
+btnContraste.addEventListener("click", () => {
+document.body.classList.toggle("alto-contraste");
+
+// Acessibilidade para leitores de tela
+const ativo = document.body.classList.contains("alto-contraste");
+btnContraste.setAttribute("aria-pressed", ativo);
+});
+
+// Função para aumentar o texto
+btnAumentar.addEventListener("click", () => {
+if (tamanhoAtualFonte < 150) {
+tamanhoAtualFonte += 10;
+document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+}
+});
+
+// Função para diminuir o texto
+btnDiminuir.addEventListener("click", () => {
+if (tamanhoAtualFonte > 90) {
+tamanhoAtualFonte -= 10;
+document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+}
+});
+});
